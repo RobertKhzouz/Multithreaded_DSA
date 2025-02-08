e9ffa6290d0f20edf0c14e01f97a2b40802e0658
RobertKhzouz <rckhzouz@gmail.com>
2025-02-08 18:40:04 -0500

    Linear Search Wrapper

--- algos.hpp
+++ algos.hpp
@@ -1,13 +1,17 @@
 #ifndef ALGOS_H
 #define ALGOS_H
 #include "structs.h"
+#include "helpers.h"
+#include <unistd.h>
+#include <random>
 #include <atomic>
 #include <mach/thread_policy.h>
 #include <mach/mach.h>
 
-/* MACOS DOES NOT SUPPORT THREAD AFFINITY -- Alternative is to encourage grouping via Affinity Tags */
+/* MACOS DOES NOT SUPPORT MANUAL THREAD AFFINITY -- Alternative is to encourage grouping via Affinity Tags */
 
-extern std::atomic<int> foundIndex;
+std::atomic<int> foundIndex(-1);
+std::atomic<pthread_t> foundThread(NULL);
 
 template <typename T>
 void *linearSearch(void *arguments)
@@ -30,11 +34,79 @@ void *linearSearch(void *arguments)
         if (args->arr[i] == args->searchVal)
         {
             foundIndex.store(i, std::memory_order_relaxed);
+            foundThread.store(pthread_self());
             return nullptr;
         }
     }
     // No thread found the value
     return nullptr;
+}
+
+template <typename T>
+void linearSearchWrapper(const int threadCount, const int arrSize, const T searchVal)
+{
+    const int sizePerThread = arrSize / threadCount;
+
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<> dist(1, 100);
+
+    // VLA not good, oops, using Vector to prevent Stack Overflow
+    std::vector<T> arr(arrSize);
+
+    pthread_t threads[threadCount];
+    int affinityTags[threadCount];
+    for (int i = 0; i < threadCount; i++)
+    {
+        affinityTags[i] = i;
+    }
+    std::vector<LinearSearchArgs<T>> argArr(threadCount);
+
+    // Random array
+    for (int i = 0; i < arrSize; i++)
+    {
+        // Genuinely despise this random number generator
+        if constexpr (std::is_same<T, std::string>::value)
+        {
+            arr.at(i) = std::to_string(dist(rng));
+        }
+        else
+        {
+            arr.at(i) = static_cast<T>(dist(rng));
+        }
+    }
+
+    // Initialize arguments -- faster than creation in loop
+    for (int i = 0; i < threadCount; i++)
+    {
+        argArr[i] = LinearSearchArgs<T>{arr.data(), i * sizePerThread, (i + 1) * sizePerThread, searchVal, affinityTags[i]};
+    }
+
+    // Thread creation + joining
+    for (int i = 0; i < threadCount; i++)
+    {
+        int errorCode = pthread_create(&threads[i], nullptr, linearSearch<T>, &argArr[i]);
+        if (errorCode)
+        {
+            std::cerr << "Error: Unable to create thread " << i << " (Error Code: " << errorCode << ")\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+    for (pthread_t thread : threads)
+    {
+        pthread_join(thread, nullptr);
+    }
+
+    // Atomic read
+    if (foundIndex.load() != -1)
+    {
+        std::cout << "Value found at index: " << foundIndex.load() << " in thread " << foundThread.load() << std::endl;
+        std::cout << "Indeed, the value at index " << foundIndex.load() << " is " << arr[foundIndex.load()] << std::endl;
+    }
+    else
+    {
+        std::cout << "Value not found!" << std::endl;
+    }
 }
 
 #endif
--- driver.cpp
+++ driver.cpp
@@ -7,13 +7,6 @@
 #include "helpers.h"
 #include "structs.h"
 
-const int ARRSIZE = 1000000;
-const int THREAD_COUNT = 4;
-const int SIZE_PER_THREAD = ARRSIZE / THREAD_COUNT;
-const int SEARCH_VAL = -1;
-
-std::atomic<int> foundIndex(-1);
-
 int main()
 {
     /*
@@ -22,43 +15,8 @@ int main()
     */
 
     auto start = std::chrono::high_resolution_clock::now();
-    int arr[ARRSIZE];
-    pthread_t threads[THREAD_COUNT];
-    int affinityTags[THREAD_COUNT] = {0, 1, 2, 3};
-    LinearSearchArgs<int> argArr[THREAD_COUNT];
-    srand(time(NULL));
 
-    // Random array
-    for (int i = 0; i < ARRSIZE; i++)
-    {
-        arr[i] = rand() % 100000;
-    }
-
-    // Initialize arguments -- faster than creation in loop
-    for (int i = 0; i < THREAD_COUNT; i++)
-    {
-        argArr[i] = LinearSearchArgs<int>{arr, i * SIZE_PER_THREAD, (i + 1) * SIZE_PER_THREAD, SEARCH_VAL, affinityTags[i]};
-    }
-
-    // Thread creation + joining
-    for (int i = 0; i < THREAD_COUNT; i++)
-    {
-        pthread_create(&threads[i], nullptr, linearSearch<int>, &argArr[i]);
-    }
-    for (pthread_t thread : threads)
-    {
-        pthread_join(thread, nullptr);
-    }
-
-    // Atomic read
-    if (foundIndex.load() != -1)
-    {
-        std::cout << "Value found at index: " << foundIndex.load() << std::endl;
-    }
-    else
-    {
-        std::cout << "Value not found!" << std::endl;
-    }
+    linearSearchWrapper(4, 100, 10);
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> duration = end - start;
